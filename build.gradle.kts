e44926f7b8e816d0e49a15b4c532e33d8fbcbef8
Ingrid Silva <ingridsilvatec@outlook.com>
2026-02-17 19:27:21 -0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.flywaydb:flyway-mysql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
