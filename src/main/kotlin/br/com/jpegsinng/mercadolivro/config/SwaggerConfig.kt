42d30290aff95cf4cc57d4c9594a381187284c9a
Ingrid Silva <ingridsilvatec@outlook.com>
2026-03-03 17:22:25 -0300

    add bearerauth to swagger

--- src/main/kotlin/br/com/jpegsinng/mercadolivro/config/SwaggerConfig.kt
+++ src/main/kotlin/br/com/jpegsinng/mercadolivro/config/SwaggerConfig.kt
@@ -1,7 +1,11 @@
 package br.com.jpegsinng.mercadolivro.config
 
+
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -11,9 +15,22 @@ class SwaggerConfig {
     @Bean
     fun customOpenAPI(): OpenAPI {
         return OpenAPI()
-            .info(Info()
-                .title("Mercado Livro")
-                .description("API do Mercado Livro")
-                .version("1.0"))
+            .info(
+                Info()
+                    .title("Mercado Livro")
+                    .description("API do Mercado Livro")
+                    .version("1.0")
+            )
+            .addSecurityItem(SecurityRequirement().addList("bearerAuth"))
+            .components(
+                Components().addSecuritySchemes(
+                    "bearerAuth",
+                    SecurityScheme()
+                        .name("bearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            )
     }
 }
